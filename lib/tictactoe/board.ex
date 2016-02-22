d2e3c5b7ed7f492cf1f1cb8f2b040d6b7553b2be
jarkyn <jarkyn@ymail.com>
2016-02-22 13:35:49 +0000

    Uses Map to represent Board

--- lib/tictactoe/board.ex
+++ lib/tictactoe/board.ex
@@ -1,17 +1,42 @@
 defmodule Tictactoe.Board do
-  import Enum, only: [map: 2, dedup: 1]
+  import Enum, only: [map: 2, dedup: 1, count: 1, reduce: 3]
+  alias Map
 
   @dimension 3
 
   def new_board do
-    map((1..size), fn(_) -> :none end)
+    reduce((0..default_size - 1), %{}, fn(position, acc) -> Map.put(acc, position, :none) end)
   end
     
   def empty?(board) do
-    dedup(board) == [:none]
+    not_present?(board, [:x, :o])
   end
 
-  defp size do
+  def full?(board) do
+    not_present?(board, [:none])
+  end
+
+  def add_move(board, position, mark) do
+    Map.put(board, position, mark)
+  end
+
+  def mark_at(board, position) do
+    Map.fetch!(board, position)
+  end
+
+  def size(board) do
+    Enum.count(board)
+  end
+
+  def default_size do
     @dimension*@dimension
+  end
+
+  defp not_present?(board, marks) do
+    !Enum.any?(board, fn(move) -> Enum.member?(marks, get_mark(move)) end)
+  end
+
+  defp get_mark(move) do
+    elem(move, 1)
   end
 end
--- test/tictactoe/board_test.exs
+++ test/tictactoe/board_test.exs
@@ -1,13 +1,38 @@
 defmodule Tictactoe.BoardTest do
   import Tictactoe.Board
-  import Enum, only: [count: 1]
+  alias Enum
+  alias IO
   use ExUnit.Case
 
   test "has default size" do
-    assert count(new_board) == 9
+    assert size(new_board) == default_size
   end
 
-  test "new board is empty" do
+  test "is empty" do
     assert empty?(new_board)
+  end
+
+  test "is full" do
+    assert full?(drawn_board)
+  end
+
+  test "adds move" do
+    board = add_move(new_board, 1, :x)
+    assert mark_at(board, 1) == :x
+  end
+
+  defp drawn_board do
+    make_board([:x, :o, :x,
+                :x, :o, :x,
+                :o, :x, :o])
+  end
+
+  defp make_board(marks) do
+    marks
+    |> Enum.with_index
+    |> Enum.reduce(new_board,
+                   fn({ mark, position }, acc) ->
+                     add_move(acc, position, mark)
+                   end)
   end
 end
