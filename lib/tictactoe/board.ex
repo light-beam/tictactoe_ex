96a1ecf111a40741533273f45f62a5ec0da01d95
jarkyn <jarkyn@ymail.com>
2016-02-23 10:45:57 +0000

    Extends Board functionality

--- lib/tictactoe/board.ex
+++ lib/tictactoe/board.ex
@@ -1,42 +1,92 @@
 defmodule Tictactoe.Board do
-  import Enum, only: [map: 2, dedup: 1, count: 1, reduce: 3]
+  alias Enum
   alias Map
 
   @dimension 3
+  @default_size @dimension*@dimension
+  @empty_mark :none
+  @player_marks [:x, :o]
+  @winning_combinations [[0, 1, 2],
+                         [3, 4, 5],
+                         [6, 7, 8],
+                         [0, 3, 6],
+                         [1, 4, 7],
+                         [2, 5, 8],
+                         [0, 4, 8],
+                         [2, 4, 6]]
 
   def new_board do
-    reduce((0..default_size - 1), %{}, fn(position, acc) -> Map.put(acc, position, :none) end)
-  end
-    
-  def empty?(board) do
-    not_present?(board, [:x, :o])
-  end
-
-  def full?(board) do
-    not_present?(board, [:none])
-  end
-
-  def add_move(board, position, mark) do
-    Map.put(board, position, mark)
+    Enum.reduce((0..@default_size - 1),
+                Map.new,
+                fn(position, acc) -> Map.put(acc, position, @empty_mark) end)
   end
 
   def mark_at(board, position) do
     Map.fetch!(board, position)
   end
 
-  def size(board) do
-    Enum.count(board)
+  def add_move(board, position, mark) do
+    Map.put(board, position, mark)
+  end
+    
+  def empty?(board) do
+    not_present?(board, @player_marks)
   end
 
-  def default_size do
-    @dimension*@dimension
+  def full?(board) do
+    not_present?(board, [@empty_mark])
+  end
+
+  def vacant_positions(board) do
+    board
+    |> Stream.filter(fn({_, mark}) -> empty_mark?(mark) end)
+    |> Enum.into(Map.new)
+    |> Map.keys
+  end
+
+  def winner(board) do
+    if line = winner_line(board) do
+      hd(line)
+    end
+  end
+
+  def active?(board) do
+    !winner(board) && !full?(board)
+  end
+
+  def vacant_position?(board, position) do
+    mark_at(board, position)
+    |> empty_mark?
+  end
+
+  defp winner_line(board) do
+    lines_for(board)
+    |> Enum.find(fn(marks) ->
+                     [ h | t ] = Enum.uniq(marks)
+                     Enum.empty?(t) && Enum.member?(@player_marks, h)
+                   end)
+  end
+
+  defp lines_for(board) do
+    @winning_combinations
+    |> Stream.map(fn(combination) ->
+                    marks_for(board, combination)
+                  end)
+  end
+
+  defp marks_for(board, positions) do
+    positions
+    |> Stream.map(fn(position) -> board[position] end)
+    |> Enum.into([])
   end
 
   defp not_present?(board, marks) do
-    !Enum.any?(board, fn(move) -> Enum.member?(marks, get_mark(move)) end)
+    !Enum.any?(board, fn({_, mark}) ->
+                        Enum.member?(marks, mark)
+                      end)
   end
 
-  defp get_mark(move) do
-    elem(move, 1)
+  defp empty_mark?(mark) do
+    mark == @empty_mark
   end
 end
--- test/tictactoe/board_test.exs
+++ test/tictactoe/board_test.exs
@@ -1,24 +1,62 @@
 defmodule Tictactoe.BoardTest do
   import Tictactoe.Board
-  alias Enum
-  alias IO
   use ExUnit.Case
 
-  test "has default size" do
-    assert size(new_board) == default_size
+  test "new board is empty" do
+    assert empty?(new_board) == true
   end
 
-  test "is empty" do
-    assert empty?(new_board)
-  end
-
-  test "is full" do
-    assert full?(drawn_board)
+  test "returns mark for a given position" do
+    assert mark_at(new_board, 0) == :none
   end
 
   test "adds move" do
     board = add_move(new_board, 1, :x)
     assert mark_at(board, 1) == :x
+  end
+
+  test "returns vacant positions" do
+    assert vacant_positions(win_board) == [5, 6, 7, 8]
+  end
+
+  test "is full" do
+    assert full?(drawn_board) == true
+  end
+
+  test "is not full" do
+    assert full?(board_with_one_mark) == false
+  end
+
+  test "returns winner if present" do
+    assert winner(win_board) == :x
+  end
+
+  test "no winner if drawn" do
+    assert winner(drawn_board) == nil
+  end
+
+  test "no winner when active" do
+    assert winner(board_with_one_mark) == nil
+  end
+
+  test "is active" do
+    assert active?(board_with_one_mark) == true
+  end
+
+  test "is not active when won" do
+    assert active?(win_board) == false
+  end
+
+  test "is not active when drawn" do
+    assert active?(drawn_board) == false
+  end
+
+  test "position is vacant" do
+    assert vacant_position?(new_board, 0) == true
+  end
+
+  test "position is not vacant" do
+    assert vacant_position?(board_with_one_mark, 0) == false
   end
 
   defp drawn_board do
@@ -27,9 +65,21 @@ defmodule Tictactoe.BoardTest do
                 :o, :x, :o])
   end
 
+  defp win_board do
+    make_board([:x,    :x,    :x,
+                :o,    :o,    :none,
+                :none, :none, :none])
+  end
+
+  def board_with_one_mark do
+    make_board([:x,    :none, :none,
+                :none, :none, :none,
+                :none, :none, :none])
+  end
+
   defp make_board(marks) do
     marks
-    |> Enum.with_index
+    |> Stream.with_index
     |> Enum.reduce(new_board,
                    fn({ mark, position }, acc) ->
                      add_move(acc, position, mark)
